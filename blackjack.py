b7ffd30df5dea6a561a6991821dfe84692a0e7f6
rohan nair <115487658+rohan-n215@users.noreply.github.com>
2023-04-30 13:41:31 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,8 @@ deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7
         'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A']
 playerHand = []
 dealerHand = []
+playerIn = True
+dealerIn = True
 
 # deal the cards
 def dealCard(turn):
@@ -20,7 +22,7 @@ def total(turn):
         if card in range(1, 11):
             total += card
         elif card in face:
-            total += 1
+            total += 10
         else:
             if total > 11:
                 total += 1
@@ -30,6 +32,48 @@ def total(turn):
 # check for winner
 def revealDealerHand():
     if len(dealerHand) == 2:
-
+        return dealerHand[0]
+    elif len(dealerHand) > 2:
+        return dealerHand[0], dealerHand[1]
 
 # game loop
+for _ in range (2):
+    dealCard(dealerHand)
+    dealCard(playerHand)
+
+while playerIn or dealerIn:
+    print(f'Dealer had {revealDealerHand()} and X')
+    print (f'You have {playerHand} for a total of {total(playerHand)}')
+    if playerIn:
+        stayOrHit = input('1: Stay\n2: Hit\n')
+    if total(dealerHand) > 16:
+        dealerIn = False
+    else:
+        dealCard(dealerHand)
+    if stayOrHit == '1':
+        playerIn = False
+    else:
+        dealCard(playerHand)
+    if total(playerHand) >= 21:
+        break
+    elif total(dealerHand) >= 21:
+        break
+
+if total(playerHand) == 21:
+    print(f'\nYou have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}')
+    print('Blackjack! You win!')
+elif total(dealerHand) == 21:
+    print(f'\nYou have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}')
+    print('Blackjack! Dealer wins!')
+elif total(playerHand) > 21:
+    print(f'\nYou have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}')
+    print('You bust! Dealer wins!')
+elif total(dealerHand) > 21:
+    print(f'\nYou have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}')
+    print('Dealer busts! You win!')
+elif 21 - total(dealerHand) < 21 - total(playerHand):
+    print(f'\nYou have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}')
+    print('Dealer wins!')
+elif 21 - total(playerHand) < 21 - total(dealerHand):
+    print(f'\nYou have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}')
+    print('You win!')
